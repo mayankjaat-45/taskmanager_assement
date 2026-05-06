7a748c96c7c236f1883e585e501b8c92ec4e1dc3
Mayank Chaudhary <mr.singh244411@gmail.com>
2026-05-06 16:16:11 +0530

    Fix file name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Tasks from "./pages/Tasks";
-import MemberDashboard from "./pages/memberDashboard";
+import MemberDashboard from "./pages/MemberDashboard";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 
